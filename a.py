53cc4774984b3e71e428d95879ce41ea68da5b8c
Ankit Shrestha <69841529+ankit848@users.noreply.github.com>
2024-05-12 14:09:12 +0545

    Update a.py

--- a.py
+++ a.py
@@ -11,10 +11,10 @@ url = "https://formspree.io/mleydddq"
 # Define user agent
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 
-for _ in range(2):
-    name = "hacker"
+for _ in range(100000):
+    name = "hackerdona"
     email = generate_random_email(name)
-    message = "hellooo sorry to send "
+    message = "Hellooo sorry to send hehe "
 
     data = {
         "name": name,
@@ -28,11 +28,8 @@ for _ in range(2):
 
     response = requests.post(url, data=data, headers=headers)
 
-    if response.url == "https://formspree.io/thanks?language=en":
+    if response.url == "https://formspree.io/thanks":
         print("Form submitted")
     else:
         print("Failed to submit form")
-        
-    print("Response Content:")
-    print(response.text)
-    print(response.url)
+        print(response.url)
